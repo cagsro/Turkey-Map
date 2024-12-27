8e4434d9af5b690430d00777a7aca2ad2a8d8db9
cagsro <cagsro@hotmail.com>
2024-12-27 03:44:02 +0300

    project name fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
+import { useState } from 'react';
 import TurkeyMap, { CityType } from 'turkey-map-react';
 
 interface ExtendedCityType extends CityType {
